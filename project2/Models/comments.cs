a72ce21cc5dbf70aa7e7b5376bdb4da3df9ffb55
AAE-hub <abody_59@hotmail.com>
2023-03-15 02:34:59 +0300

    comments model

---
+++ project2/Models/comments.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace project2.Models
+{
+
+ 
+    public class comments
+    {
+
+
+        public int Id { get; set; }
+
+        
+      
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string comment { get; set; }
+
+
+        public int articleid { get; set; }
+
+      
+    }
+  
+
+}
