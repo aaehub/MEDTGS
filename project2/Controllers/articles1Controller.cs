98e0312c90856963d4d7547fdc66b93024cc74e9
AAE-hub <abody_59@hotmail.com>
2023-03-16 23:39:52 +0300

    slider is done in customer home page

--- project2/Controllers/articles1Controller.cs
+++ project2/Controllers/articles1Controller.cs
@@ -26,6 +26,11 @@ namespace project2.Controllers
         {
               return View(await _context.article.ToListAsync());
         }
+
+
+
+
+
        
         // GET: articles1/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -291,5 +296,32 @@ namespace project2.Controllers
         {
           return _context.article.Any(e => e.Id == id);
         }
+
+
+
+        public async Task<IActionResult> slider()
+        {
+
+            return View(await _context.article.ToListAsync());
+
+
+        }
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
